9adbc8cae36f3a537194f03db9abd854c10e85bc
Ankara-mg <solroman44@gmail.com>
2025-08-26 17:12:54 -0300

    chore: agregar port a env

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4200,
-    proxy: {
-      "/api": "http://localhost:3000"
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT || 5173,
+      proxy: {
+        "/api": "http://localhost:3000"
+      }
     }
-  }
-});
+  })
+};
